eaf264d17d781598550b88a8948ad915941e35f7
Ian Mackay <ianmackay00@gmail.com>
2019-01-03 18:12:48 -0800

    Update lazy_update.sh

--- lazy_update.sh
+++ lazy_update.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 NAME="github-desktop"
 
-cp "../${NAME}/PKGBUILD" "./rel/"
-cp "../${NAME}/.SRCINFO" "./rel/"
-cp "../${NAME}/${NAME}.desktop" "./rel/"
-cp "../${NAME}-git/PKGBUILD" "./git/"
-cp "../${NAME}-git/.SRCINFO" "./git/"
-cp "../${NAME}-git/${NAME}.desktop" "./git/"
-cp "../${NAME}-bin/PKGBUILD" "./bin/"
-cp "../${NAME}-bin/.SRCINFO" "./bin/"
-cp "../${NAME}-bin/${NAME}.desktop" "./bin/"
+if [ $1 = "-F" ]
+then
+  cp "../${NAME}/PKGBUILD" "./rel/"
+  cp "../${NAME}/.SRCINFO" "./rel/"
+  cp "../${NAME}/${NAME}.desktop" "./rel/"
+  cp "../${NAME}-git/PKGBUILD" "./git/"
+  cp "../${NAME}-git/.SRCINFO" "./git/"
+  cp "../${NAME}-git/${NAME}.desktop" "./git/"
+  cp "../${NAME}-bin/PKGBUILD" "./bin/"
+  cp "../${NAME}-bin/.SRCINFO" "./bin/"
+  cp "../${NAME}-bin/${NAME}.desktop" "./bin/"
+elif [ $1 = "-T" ]
+then
+  cp "./rel/PKGBUILD" "../${NAME}/"
+  cp "./rel/.SRCINFO" "../${NAME}/"
+  cp "./rel/${NAME}.desktop" "../${NAME}/"
+  cp "./git/PKGBUILD" "../${NAME}-git/"
+  cp "./git/.SRCINFO" "../${NAME}-git/"
+  cp "./git/${NAME}.desktop" "../${NAME}-git/"
+  cp "./bin/PKGBUILD" "../${NAME}-bin/"
+  cp "./bin/.SRCINFO" "../${NAME}-bin/"
+  cp "./bin/${NAME}.desktop" "../${NAME}-bin/"
+else
+  echo "Usage: ./lazy_update.sh [-F|-T] (from or to)"
+fi
